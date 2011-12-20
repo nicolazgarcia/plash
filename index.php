80aeadf33a91e637d8bcd057e81e4b79786d190c
Mauricio Mantish <Mao@.(none)>
2011-12-20 13:00:05 -0500

    script del footer en su propio archivo y con rutas de las imagenes corregidas

--- index.php
+++ index.php
@@ -14,10 +14,11 @@
 	wp_enqueue_script('ProgressiveImageSequence', get_bloginfo('template_directory').'/js/ProgressiveImageSequence.class.js', array('jquery'));
 	wp_enqueue_script('stickytitle', get_bloginfo('template_directory').'/js/stickytitle.js', array('jquery'));
 	wp_enqueue_script('scripts', get_bloginfo('template_directory').'/js/scripts.js', array('jquery'));
+	wp_enqueue_script('footer', get_bloginfo('template_directory').'/js/footer.js', array('jquery'), '', true);
 	wp_head();
 	?>
 </head>
-<body <?php body_class(); ?>>
+<body <?php body_class(); ?> data-blogurl="<?php echo get_bloginfo('wpurl');?>">
 <div id="main">
 <div class="video_lasplash" style="z-index:-999;"><img src="<?php bloginfo( 'template_directory' ); ?>/images/bg/video-0001.jpg"></div>
    <div class="navigation">
@@ -571,130 +572,6 @@
   </div>
   <!--/cont-->  
 </div>
-<script>
-		$(document).ready(function(){
-			var $doc = $(document);
-			var $win = $(window);
-			// dimensions - we want to cache them on window resize
-			var windowHeight, windowWidth;
-			var fullHeight, scrollHeight;
-			var streetImgWidth = 520, streetImgHeight = 545;
-			calculateDimensions();
-
-			var currentPosition = -1, targetPosition = 0;
-			var $videoContainer = $('.video_lasplash');
-			var video = $('.video_lasplash > img')[0];
-			var $hotspotElements = $('[data-position]');
-			// handling resize and scroll events			
-			function calculateDimensions() {
-				windowWidth = $win.width();
-				windowHeight = $win.height();
-				fullHeight = $('#main').height();
-				scrollHeight = fullHeight - windowHeight;
-			}			
-			function handleResize() {
-				calculateDimensions();
-				resizeBackgroundImage();
-				handleScroll();
-			}			
-			function handleScroll() {
-				targetPosition = $win.scrollTop() / scrollHeight;
-			}			
-			// main render loop
-			window.requestAnimFrame = (function(){
-			  return  window.requestAnimationFrame       ||
-			          window.webkitRequestAnimationFrame ||
-			          window.mozRequestAnimationFrame    ||
-			          window.oRequestAnimationFrame      ||
-			          window.msRequestAnimationFrame     ||
-			          function(/* function */ callback, /* DOMElement */ element){
-			            window.setTimeout(callback, 1000 / 60);
-			          };
-			})();
-			function animloop(){
-				if ( Math.floor(currentPosition*5000) != Math.floor(targetPosition*5000) ) {
-					currentPosition += (targetPosition - currentPosition) / 5;
-					render(currentPosition);
-				}
-			  requestAnimFrame(animloop);
-			}
-			// rendering
-			function render( position ) {
-				// position the elements
-				var minY = -windowHeight, maxY = windowHeight;
-				$.each($hotspotElements,function(index,element){
-					var $hotspot = $(element);
-					var elemPosition = Number( $hotspot.attr('data-position') );
-					var elemSpeed = Number( $hotspot.attr('data-speed') );
-					var elemY = windowHeight/2 + elemSpeed * (elemPosition-position) * scrollHeight;
-					if ( elemY < minY || elemY > maxY ) {
-						$hotspot.css({'visiblity':'none', top: '-1000px','webkitTransform':'none'});
-					} else {
-						$hotspot.css({'visiblity':'visible', top: elemY, position: 'fixed'});
-					}
-				});				
-				renderVideo( position );
-			}
-			function resizeBackgroundImage(){
-				// get image container size
-				var scale = Math.max( windowHeight/streetImgHeight , windowWidth/streetImgWidth );
-				var width = scale * streetImgWidth , height = scale * streetImgHeight;
-				var left = (windowWidth-width)/2, top = (windowHeight-height)/2;
-				$videoContainer
-						  .width(width).height(height)
-						  .css('position','fixed')
-						  .css('left',left+'px')
-						  .css('top',top+'px');
-			}
-			// video handling
-			var imageSeqLoader = new ProgressiveImageSequence( "images/bg/video-{index}.jpg" , 961 , {
-				indexSize: 4,
-				initialStep: 16,
-				onProgress: handleLoadProgress,
-				onComplete: handleLoadComplete,
-				stopAt: 1
-			} );
-			var loadCounterForIE = 0; // there seems to be a problem with ie calling the callback several times
-			imageSeqLoader.loadPosition(currentPosition,function(){
-				loadCounterForIE++;
-				if ( loadCounterForIE == 1 ) {
-					renderVideo(currentPosition);
-					imageSeqLoader.load();
-					imageSeqLoader.load();
-					imageSeqLoader.load();
-					imageSeqLoader.load();
-				}
-			});
-			var currentSrc, currentIndex;
-			function renderVideo(position) {
-				var index = Math.round( currentPosition * (imageSeqLoader.length-1) );
-				var img = imageSeqLoader.getNearest( index );
-				var nearestIndex = imageSeqLoader.nearestIndex;
-				if ( nearestIndex < 0 ) nearestIndex = 0;
-				var $img = $(img);
-				var src;
-				if ( !!img ) {
-					src = img.src;
-					if ( src != currentSrc ) {
-						video.src = src;
-						currentSrc = src;
-					}
-				}
-			}
-			$('body').append('<div id="loading-bar" style="position:fixed; bottom:0; left:0; background-color: #DF0012; background-color: rgba(223,0,18,0.5); height: 1px;"></div>');			
-			function handleLoadProgress() {
-				var progress = imageSeqLoader.getLoadProgress() * 100;
-				$('#loading-bar').css({width:progress+'%',opacity:1});
-			}
-			function handleLoadComplete() {
-				$('#loading-bar').css({width:'100%',opacity:0});
-			}
-			$win.resize( handleResize );
-			$win.scroll( handleScroll );
-			handleResize();
-			animloop();
-		});
-</script>
 <?php
 	wp_footer();
 ?>

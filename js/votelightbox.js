fb582f47228f528e71f0f2c7457713d800ca9e41
Mauricio Mantish <mauriciomantilla@gmail.com>
2012-02-16 21:36:34 -0500

    Lista la funcionalidad de los votos

--- js/votelightbox.js
+++ js/votelightbox.js
@@ -4,6 +4,9 @@
 	{
 		var openLightBox = function(e)
 		{
+			if ( $('.wp-polls-form').length == 0 ){
+				$('#big-pic input').replaceWith('<p>Ya has votado por tu amiga, ¡cuéntale a más personas para que tu amiga sea finalista!</p>');
+			}
 			var img_src = $(this).parent().attr('data-img-src');
 			$('#big-pic img').attr('src', img_src);
 			$('#lightbox').fadeIn();
@@ -14,8 +17,21 @@
 			$('#lightbox').fadeOut();
 		}
 
+		var sendVote = function(e)
+		{
+			var poll_id = $('.wp-polls-form input[name=poll_id]').val();
+			poll_vote(2);
+
+			$('#big-pic input').ajaxSuccess(function(e, xhr, settings) {
+				if (xhr.status == 200){
+					$(this).replaceWith('<p>¡Gracias! Tu voto ha sido enviado exitosamente</p>');
+				}
+			});
+		}
+
 		$('.wp-polls-ul input').on('click', openLightBox);
 		$('#close-box').on('click', closeLightBox);
+		$('#big-pic input').on('click', sendVote);
 	}
 
 	$(function(){
